defb073297f6041137bbee995d0f1ed5c83b7c5e
dg978t <dg978t@att.com>
2018-10-31 02:50:04 -0500

    Login change

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from './login.service';
+import { LoginService } from './login.service'
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -12,23 +12,26 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class LoginComponent implements OnInit {
   message = 'Logging in..';
   constructor(private loginService: LoginService, private route: ActivatedRoute,
-    private router: Router) {
-     }
+    private router: Router) { }
 
   ngOnInit() {
-    this.loginService.logon().subscribe(resp => {
-      console.log(resp);
-      if ( !resp ) {
-        window.location.href = 'https://www.e-access.att.com/empsvcs/hrpinmgt/pagLogin/?sysName=usrp&retURL=' + window.location.href;
-      } else {
-        localStorage.setItem('attuid', resp['attuid']);
-        localStorage.setItem('firstName', resp['firstName']);
-        localStorage.setItem('lastName', resp['lastName']);
-        localStorage.setItem('roles', JSON.stringify(resp['roles']));
-        const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-        this.router.navigateByUrl(returnUrl);
-      }
-    });
+    // this.loginService.logon().subscribe(resp => {
+    //   if ( !resp ) {
+    //     window.location.href = 'https://www.e-access.att.com/empsvcs/hrpinmgt/pagLogin/?sysName=usrp&retURL=' + window.location.href;
+    //   } else {
+    //     localStorage.setItem('attuid', resp['attuid']);
+    //     localStorage.setItem('firstName', resp['firstName']);
+    //     localStorage.setItem('lastName', resp['lastName']);
+    //     localStorage.setItem('roles', JSON.stringify(resp['roles']));
+    //     const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    //     this.router.navigateByUrl(returnUrl);
+    //   }
+    // });
+    localStorage.setItem('attuid', 'dg978t');
+    localStorage.setItem('firstName', 'David');
+    localStorage.setItem('lastName', 'Gonzalez');
+    const returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.router.navigateByUrl(returnUrl);
   }
 
 }
